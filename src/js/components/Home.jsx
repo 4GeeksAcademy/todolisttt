61bc14dcd5e0a1383a97dae8e3abed6f89ea9770
albertoZG <albertozg94@gmail.com>
2025-07-24 23:33:19 +0000

    cambios2

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,87 +1,133 @@
 import React, { useState, useEffect } from "react";
 import ListaTarea from "./ListaTarea";
 
-const API_URL = `https://playground.4geeks.com/todo/todos/alberto`;
+// const API_URL = `https://playground.4geeks.com/todo/users/alberto`;
 
 const Home = () => {
-  const [tarea, setTarea] = useState("");
-  const [tareas, setTareas] = useState([]);
+  const [tarea, setTarea] = useState([]);
+  const [tareas, setTareas] = useState([])
 
 
-  const crearLaTarea = async () => {
-    try {
-      const response = await fetch(API_URL, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify([]),
-      });
-      if (!response.ok) {
-        console.log("No se pudo crear la tarea");
-      } else {
-        console.log("Tarea creada");
-      }
-    } catch (error) {
-      console.log("Error creando tarea:", error.message);
+  const crearTarea = async () => {
+    const response = await fetch("https://playground.4geeks.com/todo/users/alberto", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify()
+    })
+  }
+
+  const getTareas = async () => {
+    const response = await fetch("https://playground.4geeks.com/todo/users/alberto")
+    console.log(response);
+    if (!response.ok) {
+      console.log("crea la tarea");
+      cargarTareas();
+      return
+
     }
-  };
 
+    const data = await response.json()
+    console.log(data);
+    setTareas(lista);
 
+  }
+
+  const cargarTareas = async () => {
+    const response = await fetch("https://playground.4geeks.com/todo/todos/alberto", {
+      method: "POST"
+    })
+    console.log(response);
+    const data = await response.json()
+    console.log(data);
+    getTareas()
+  }
+
+  // getTareas()
   useEffect(() => {
-    const cargarTareas = async () => {
-      try {
+    getTareas()
+  }, [])
 
-        const response = await fetch(API_URL, {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify([]),
-        });
 
-        if (response.status === 404) {
-          console.log("Usuario no existe, creando...");
-          await crearLaTarea();
-          setTareas([]);
-          return;
-        }
 
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
 
-        const data = await response.json();
-        const lista = data.todos.map((t) => t.label);
-        setTareas(lista);
-      } catch (error) {
-        console.log("Error cargando tareas:", error.message);
-      }
-    };
 
-    cargarTareas();
-  }, []);
+
+  // const crearLaTarea = async () => {
+  //   try {
+  //     const response = await fetch(API_URL, {
+  //       method: "POST",
+  //       headers: { "Content-Type": "application/json" },
+  //       body: JSON.stringify([]),
+  //     });
+  //     if (!response.ok) {
+  //       console.log("No se pudo crear la tarea");
+  //     } else {
+  //       console.log("Tarea creada");
+  //     }
+  //   } catch (error) {
+  //     console.log("Error creando tarea:", error.message);
+  //   }
+  // };
+
+
+  // useEffect(() => {
+  //   const cargarTareas = async () => {
+  //     try {
+
+  //       const response = await fetch(API_URL, {
+  //         method: "POST",
+  //         headers: { "Content-Type": "application/json" },
+  //         body: JSON.stringify([]),
+  //       });
+
+  //       if (response.status === 404) {
+  //         console.log("Usuario no existe, creando...");
+  //         await crearLaTarea();
+  //         setTareas([]);
+  //         return;
+  //       }
+
+  //       if (!response.ok) {
+  //         throw new Error(`HTTP error! status: ${response.status}`);
+  //       }
+
+  //       const data = await response.json();
+  //       const lista = data.todos.map((t) => t.label);
+  //       setTareas(lista);
+  //     } catch (error) {
+  //       console.log("Error cargando tareas:", error.message);
+  //     }
+  //   };
+
+  //   cargarTareas();
+  // }, []);
 
 
   const guardarTareas = async (lista) => {
     setTareas(lista);
 
-    try {
-      const response = await fetch(API_URL, {
-        method: "PUT",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(
-          lista.map((label) => ({
-            label,
-            done: false,
-          }))
-        ),
-      });
+    //   try {
+    //     const response = await fetch(API_URL, {
+    //       method: "PUT",
+    //       headers: { "Content-Type": "application/json" },
+    //       body: JSON.stringify(
+    //         lista.map((label) => ({
+    //           label,
+    //           done: false,
+    //         }))
+    //       ),
+    //     });
 
-      if (!response.ok) {
-        console.log("Error guardando tareas");
-      } else {
-        console.log("Tareas guardadas correctamente");
-      }
-    } catch (error) {
-      console.log("Error en guardar tareas:", error.message);
-    }
+    // if (!response.ok) {
+    //   console.log("Error guardando tareas");
+    // } else {
+    //   console.log("Tareas guardadas correctamente");
+    // }
+    //   } catch (error) {
+    //     console.log("Error en guardar tareas:", error.message);
+    //   }
   };
 
 
